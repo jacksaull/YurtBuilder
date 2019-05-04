af135fe161c1f2b1676c99c5597cde5e106cadd6
jacksaull <32143271+jacksaull@users.noreply.github.com>
2019-05-04 05:03:59 +0100

    Yurt Wall and Roof Colour changing added

---
+++ Unity/YurtBuildingApplication/Assets/RoofColour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class RoofColour : MonoBehaviour
+{
+    public Material[] RoofColours = new Material[3];
+    public GameObject YurtRoof;
+    private string Colour;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeColour()
+    {
+        Colour = GetComponent<Button>().name;
+
+        if (Colour == "Cream")
+        {
+            YurtRoof.GetComponent<Renderer>().material = RoofColours[0];
+        }
+        else if (Colour == "Sand")
+        {
+            YurtRoof.GetComponent<Renderer>().material = RoofColours[1];
+        }
+        else if (Colour == "Olive")
+        {
+            YurtRoof.GetComponent<Renderer>().material = RoofColours[2];
+        }
+    }
+}
---
+++ Unity/YurtBuildingApplication/Assets/WallColour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WallColour : MonoBehaviour
+{
+    public Material[] WallColours = new Material[3];
+    public GameObject YurtWall;
+    private string Colour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColour()
+    {
+        Colour = GetComponent<Button>().name;
+
+        if (Colour == "Cream")
+        {
+            YurtWall.GetComponent<Renderer>().material = WallColours[0];
+        }
+        else if (Colour == "Sand")
+        {
+            YurtWall.GetComponent<Renderer>().material = WallColours[1];
+        }
+        else if (Colour == "Olive")
+        {
+            YurtWall.GetComponent<Renderer>().material = WallColours[2];
+        }
+    }
+}
