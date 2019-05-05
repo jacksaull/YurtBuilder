27894ba502b78aba23eb58374200277fdf9bdf31
jacksaull <32143271+jacksaull@users.noreply.github.com>
2019-05-06 00:46:29 +0100

    Glass Cap and 3 Floor Types added

---
+++ Unity/YurtBuildingApplication/Assets/FloorMaterial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class FloorMaterial : MonoBehaviour
+{
+    public Material[] FloorMaterials = new Material[3];
+    public GameObject YurtFloor;
+    private string Materials;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeColour()
+    {
+        Materials = GetComponent<Button>().name;
+
+        if (Materials == "Carpet")
+        {
+            YurtFloor.GetComponent<Renderer>().material = FloorMaterials[0];
+        }
+        else if (Materials == "Hardwood")
+        {
+            YurtFloor.GetComponent<Renderer>().material = FloorMaterials[1];
+        }
+        else if (Materials == "Spiderweb")
+        {
+            YurtFloor.GetComponent<Renderer>().material = FloorMaterials[2];
+        }
+    }
+}
